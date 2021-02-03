f0c23967cf0820144e8cfb6da3e25182841db732
will <liudongling@bolo.me>
2021-02-03 16:52:49 +0800

    fix

--- common.go
+++ common.go
@@ -1,18 +1,45 @@
 package proto
 
 import (
+	"fmt"
+	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
 	grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
 	"github.com/sercand/kuberesolver/v3"
 	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/balancer/roundrobin"
+	"net"
 )
 
 var entry = logrus.NewEntry(logrus.StandardLogger())
 
 func GetLogger() *logrus.Entry {
 	return entry
+}
+func DefaultGrpcServer(serviceName string, chainStreamServer []grpc.StreamServerInterceptor, chainUnaryServer []grpc.UnaryServerInterceptor) *grpc.Server {
+	grpc_logrus.ReplaceGrpcLogger(GetLogger())
+	chainStreamServer = append(chainStreamServer, []grpc.StreamServerInterceptor{grpc_logrus.StreamServerInterceptor(GetLogger()),
+		grpc_opentracing.StreamServerInterceptor(),
+	}...)
+
+	chainUnaryServer = append(chainUnaryServer, []grpc.UnaryServerInterceptor{grpc_logrus.UnaryServerInterceptor(GetLogger()),
+		grpc_opentracing.UnaryServerInterceptor(),
+	}...)
+	return grpc.NewServer(
+		grpc.StreamInterceptor(
+			grpc_middleware.ChainStreamServer(chainStreamServer...)),
+		grpc.UnaryInterceptor(
+			grpc_middleware.ChainUnaryServer(chainUnaryServer...)),
+	)
+}
+
+func Run(s *grpc.Server, serverAddress string) error {
+	lis, err := net.Listen("tcp", serverAddress)
+	if err != nil {
+		return fmt.Errorf("failed to listen %w", err)
+	}
+	return s.Serve(lis)
 }
 
 func DefaultConn(target string) (*grpc.ClientConn, error) {
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.4.3
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
 	github.com/sercand/kuberesolver/v3 v3.0.0
+	github.com/sirupsen/logrus v1.4.2
 	google.golang.org/grpc v1.35.0
 	google.golang.org/protobuf v1.25.0
 )
